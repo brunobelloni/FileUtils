b8f795bf27c31adccc187cfdda1928493faa811b
brunobelloni <bruno.belloni@hotmail.com>
2019-08-31 16:17:54 -0300

    optional time argparse

--- rename_files.py
+++ rename_files.py
@@ -1,14 +1,17 @@
 # USAGE: python rename_files.py 'dir'
+# USAGE: python rename_files.py --dir 'DIR' --time True
 # Rename Files order by ID
 
 # Shell
 # num=0; for i in *; do mv "$i" "$(printf '%04d' $num).${i#*.}"; ((num++)); done
 
-import os
 import argparse
+import datetime
+import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument("dir", help="dir")
+parser.add_argument("--dir", help="dir")
+parser.add_argument("--time", help="time", default=False, type=bool)
 args = parser.parse_args()
 
 if args.dir:
@@ -18,5 +21,8 @@ if args.dir:
 
     for index, arquivo in enumerate(arquivos):
         name = "{:05}.png".format(index)
+        if args.time:
+            time = datetime.datetime.now().time().strftime("%Y-%m-%d-%I-%M-%S-%p")
+            name = "{:05}-{}.png".format(index, time)
         new_name = os.path.join(os.path.dirname(arquivo), name)
         os.rename(arquivo, new_name)
