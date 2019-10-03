5560d6216504f8f65e0b527aa23d42010dd8dee3
brunobelloni <bruno.belloni@hotmail.com>
2019-10-03 19:34:06 -0300

    return None

--- file_utils/api.py
+++ file_utils/api.py
@@ -1,20 +1,18 @@
-from pathlib import Path
 import glob
 
 
 class FileUtils:
-
     def __init__(self, path: str):
         self.path = path
 
     def get_all_files_in_path(self, file_type: str = '*', recursive: bool = False) -> list:
-        return [f for f in glob.glob(self.path + f'**/*.{file_type}', recursive=recursive)]
+        return [f for f in glob.glob(pathname=self.path + f'**/*.{file_type}', recursive=recursive)]
 
-    def rename_all_files(self):
+    def rename_all_files(self) -> None:
         pass
 
-    def delete_duplicated_images(self):
+    def delete_duplicated_images(self) -> None:
         pass
 
-    def image_slice(self):
+    def image_slice(self) -> None:
         pass
